53c2ce85ec2a600e9f91ac855d9e5f00763a3d73
Siddhartha <siddu.siddhartha@gmail.com>
2023-02-20 17:07:11 +0000

    import

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,5 @@ export * from "./app-check/index.js";
 export * from "./auth/index.js";
 export * from "./common/index.js";
 export * from "./database/index.js";
-export * from "./firestore/index.js";
 export * from "./messaging/index.js";
 export * from "./storage/index.js";
